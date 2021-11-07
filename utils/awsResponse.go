4b97227aa551a006e1ec06a6ec6988af442edc61
gmlalfjr <gamalfajri@gmail.com>
2021-11-07 21:37:10 +0700

    ad struct

--- utils/awsResponse.go
+++ utils/awsResponse.go
@@ -34,3 +34,23 @@ func FailResponse(data *ErrorWrapper) (events.APIGatewayProxyResponse, error) {
 		StatusCode: data.StatusCode,
 	}, nil
 }
+
+func SuccessResponsePagination(data *SuccessPaginationWrapper) (events.APIGatewayProxyResponse, error) {
+	body, err := json.Marshal(data)
+	if err != nil {
+		return FailResponse(&ErrorWrapper{
+			Error:      err.Error(),
+			Message:    "Gor Error When Marshaling",
+			StatusCode: 400,
+		})
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode:      data.StatusCode,
+		IsBase64Encoded: false,
+		Body:            string(body),
+		Headers: map[string]string{
+			"Content-Type": "application/json",
+		},
+	}, nil
+}
